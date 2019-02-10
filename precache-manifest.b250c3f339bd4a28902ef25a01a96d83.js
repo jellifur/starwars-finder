924cf096c7ed21266ae9a26371a46c13008044dc
Jenn Chan <chanj@MacBook-Pro.local>
2019-02-09 19:16:43 -0800

    Updates

---
+++ precache-manifest.b250c3f339bd4a28902ef25a01a96d83.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff9b2dae1ec88e13c183",
+    "url": "/starwars-finder/static/css/main.7a850290.chunk.css"
+  },
+  {
+    "revision": "ff9b2dae1ec88e13c183",
+    "url": "/starwars-finder/static/js/main.ff9b2dae.chunk.js"
+  },
+  {
+    "revision": "e0c98c837dcf560b6928",
+    "url": "/starwars-finder/static/js/1.e0c98c83.chunk.js"
+  },
+  {
+    "revision": "db0ca2a855e2e5146628",
+    "url": "/starwars-finder/static/js/runtime~main.db0ca2a8.js"
+  },
+  {
+    "revision": "1f7cefb58d2507b130867eb3ac1a8ce2",
+    "url": "/starwars-finder/index.html"
+  }
+];
