13a47a4248d0ac88062295ddf6af72a436e31085
Jenn Chan <chanj@MacBook-Pro.local>
2019-02-09 19:45:21 -0800

    Updates

---
+++ precache-manifest.072a703ece64e839974f5eb2de0d5a46.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "de737c890709780cf989",
+    "url": "/starwars-finder/static/css/main.56297465.chunk.css"
+  },
+  {
+    "revision": "de737c890709780cf989",
+    "url": "/starwars-finder/static/js/main.de737c89.chunk.js"
+  },
+  {
+    "revision": "e0c98c837dcf560b6928",
+    "url": "/starwars-finder/static/js/1.e0c98c83.chunk.js"
+  },
+  {
+    "revision": "db0ca2a855e2e5146628",
+    "url": "/starwars-finder/static/js/runtime~main.db0ca2a8.js"
+  },
+  {
+    "revision": "9566b43ae17ef7e8ce3db473240a4b03",
+    "url": "/starwars-finder/static/media/space.9566b43a.jpg"
+  },
+  {
+    "revision": "f059944a68e6a97a7ec3ef91cd553798",
+    "url": "/starwars-finder/static/media/person.f059944a.svg"
+  },
+  {
+    "revision": "006b97cddd4bead3a9ea42158ca130c4",
+    "url": "/starwars-finder/static/media/search.006b97cd.svg"
+  },
+  {
+    "revision": "89daa05ca52c53cf78fe883ec786608d",
+    "url": "/starwars-finder/index.html"
+  }
+];
